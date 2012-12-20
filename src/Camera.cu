67ee20de9675c7dc0a11235f836456e200b8e6da
Carlos Duelo Serrano <cduelo@espino.datsi.fi.upm.es>
2012-12-20 12:50:11 +0100

    optimized ray computation

--- src/Camera.cu
+++ src/Camera.cu
@@ -4,11 +4,10 @@
  */
 
 #include "Camera.hpp"
+#include "cuda_help.hpp"
 #include <math.h>
 #include <iostream>
 #include <fstream>
-
-#define BLOCK_SIZE 16
 
 Camera::Camera(int nRP, int p_H, int p_W, float p_d, float p_fov_h, float p_fov_w) //inits the values (Position: (0|0|0) Target: (0|0|-1) )
 {
@@ -39,89 +38,28 @@ Camera::~Camera()
 
 __global__ void cuda_updateRays(float3 * rays, int numRayPixel, float3 up, float3 right, float3 look, int H, int W, float h, float w, float distance, float fov_H, float fov_W, float3 position)
 {
-		#if 0 //XXX CPU CODE
-		float ih  = screen->h/screen->H;
-		float iw  = screen->w/screen->W;
+	int id = blockIdx.y * blockDim.x * gridDim.y + blockIdx.x * blockDim.x +threadIdx.x;
 
-		vmml::vector<3, float> A = position + look * screen->distance;
-		A += up * ((screen->h/2.0) + ih/2.0);
-		A -= right * ((screen->w/2.0) + iw/2.0);
-		for(int i=0; i < screen->H; i++)
-			for(int j=0; j < screen->W; j++)
-			{
-				rayDirections[i*screen->W+j] = (A + (j*iw)*right - (i*ih*up)) - position;
-				rayDirections[i*screen->W+j].normalize();
-			}
-		#endif
-
-	int i  = blockIdx.x * blockDim.x + threadIdx.x;
-	int j  = blockIdx.y * blockDim.y + threadIdx.y;
-	//int id = j * H + i;
-	int id = i * W + j;
-
-	float ih  = h/H;
-	float iw  = w/W;
-
-	float3 A = position + (look * distance);
-	A += up * ((h/2.0) + ih/2.0);
-	A -= right * ((w/2.0) + iw/2.0);
-	A += (j*iw)*right; 
-	A -= (i*ih)*up;
-	A -= position;
-	rays[id] = normalize(A);
-
-	#if 0
-	int i  = blockIdx.x * blockDim.x + threadIdx.x;
-	int j  = blockIdx.y * blockDim.y + threadIdx.y;
-
-	int id = i * W + j;
-	id *= numRayPixel*numRayPixel;
-	
-	float ih  = h/H;
-	float iw  = w/W;
-
-	float3 ph;
-	float3 pw;
-	float fov_H_PI = distance * tanf(fov_H*(M_PI/180.0));
-	float fov_W_PI = distance * tanf(fov_W*(M_PI/180.0));
-	ph = fov_H_PI * up;
-	pw = fov_W_PI * right;
-	float  ihr             = ih/(2.0*numRayPixel);
-	float  iwr             = iw/(2.0*numRayPixel);
-	float  matDisPixel_W[4][4];
-	float  matDisPixel_H[4][4];
-	for(int rpW = 0; rpW<numRayPixel; rpW++)
+	if (id < (W*H))
 	{
-		for(int rpH = 0; rpH<numRayPixel; rpH++)
-		{
-			matDisPixel_H[rpW][rpH] = (2.0*rpH*ihr+ihr);
-			matDisPixel_W[rpW][rpH] = (2.0*rpW*iwr+iwr);
-		}
-	}
-	int indice2 = 0;
-	for(int rpW = 0; rpW<numRayPixel; rpW++)
-	{
-		for(int rpH = 0; rpH<numRayPixel; rpH++)
-		{
-			//rays[id + indice2].x = i;
-			//rays[id + indice2].y = j;
-			
-			rays[id + indice2]         = (distance * look) + ph - pw;
-			rays[id + indice2].x      -= i*ih+matDisPixel_H[rpW][rpH];
-			rays[id + indice2].y      -= j*iw+matDisPixel_W[rpW][rpH];
-			rays[id + indice2] 	= gpu_normalize(rays[id + indice2]);
-			
-			indice2++;
-		}
-	}
-	#endif
+		int i  = id / W;
+		int j  = id % W;
 
+		float ih  = h/H;
+		float iw  = w/W;
+
+		float3 A = (look * distance);
+		A += up * ((h/2.0f) - (ih*(i + 0.5f)));
+		A += right * (-(w/2.0f) + (iw*(j + 0.5f)));
+
+		rays[id] = normalize(A);
+	}
 }
 
 void Camera::UpdateRays()
 {
-	dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
-	dim3 blocks(screen->get_H()/BLOCK_SIZE, screen->get_W()/BLOCK_SIZE);
+	dim3 threads = getThreads(numRays);
+	dim3 blocks = getBlocks(numRays);
 	std::cerr<<"Launching kernek ray generation blocks ("<<blocks.x<<","<<blocks.y<<","<<blocks.z<<") threads ("<<threads.x<<","<<threads.y<<","<<threads.z<<") error: "<< cudaGetErrorString(cudaGetLastError())<<std::endl;
 	cuda_updateRays<<<blocks, threads>>>(rayDirections, numRayPixel, up, right, look, screen->get_H(), screen->get_W(), screen->get_h(), screen->get_w(), screen->get_Distance(), screen->get_fovH(), screen->get_fovW(), position);
 	std::cerr<<"Synchronizing calculating rays: " << cudaGetErrorString(cudaDeviceSynchronize()) << std::endl;
